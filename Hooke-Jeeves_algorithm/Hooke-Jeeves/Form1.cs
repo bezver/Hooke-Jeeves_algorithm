d94d7545caebb3c24de4b47ca240ee2fe6901e2c
bezver <60857455+bezver@users.noreply.github.com>
2021-03-10 13:21:25 +0200

    Initial commit

--- Hooke-Jeeves_algorithm/Hooke-Jeeves/Form1.cs
+++ Hooke-Jeeves_algorithm/Hooke-Jeeves/Form1.cs
@@ -16,5 +16,10 @@ namespace Hooke_Jeeves
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }

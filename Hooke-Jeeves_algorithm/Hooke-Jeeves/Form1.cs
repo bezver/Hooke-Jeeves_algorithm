d269d2bb835751caebb8236343f6f1039886c12d
bezver <60857455+bezver@users.noreply.github.com>
2021-03-10 13:28:03 +0200

    Start

--- Hooke-Jeeves_algorithm/Hooke-Jeeves/Form1.cs
+++ Hooke-Jeeves_algorithm/Hooke-Jeeves/Form1.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using info.lundin.math;
+
 namespace Hooke_Jeeves
 {
     public partial class Form1 : Form
---
+++ Hooke-Jeeves_algorithm/Hooke-Jeeves/Solver.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using info.lundin.math;
+
+namespace Hooke_Jeeves
+{
+    class Solver
+    {
+        public static double Function(List<string> paramNames, List<double> paramValues, string formula)
+        {
+            if (paramNames.Count != paramValues.Count)
+            {
+                throw new InvalidProgramException("Error in function parameters");
+            }
+            ExpressionParser parser = new ExpressionParser();
+
+            for (int i = 0; i < paramNames.Count; i++)
+            {
+                parser.Values.Add(paramNames[i], paramValues[i]);
+            }
+            return parser.Parse(formula);
+        }
+    }
+}
